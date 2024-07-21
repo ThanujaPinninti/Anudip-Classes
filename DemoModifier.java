3831525feef10ecffd6f6d78666ddb79225e5249
Thanuja <156277448+ThanujaPinninti@users.noreply.github.com>
2024-07-21 21:55:09 +0530

    Using Modifier

---
+++ DemoModifier.java
@@ -0,0 +1,15 @@
+public class DemoModifier {
+    public int num1;
+    private int num2;
+    protected int num3;
+    int num4;
+    public int getNum2(){
+        return num2;
+
+    }
+    public void setNum2(int num2){
+        this.num2=num2;
+    }
+
+    
+}
