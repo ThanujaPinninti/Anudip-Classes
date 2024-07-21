e2f41c9bba00d432738d8f8e6aa5aaa12e26a673
Thanuja <156277448+ThanujaPinninti@users.noreply.github.com>
2024-07-21 22:24:22 +0530

    Do while Program Example

---
+++ Dowhile.java
@@ -0,0 +1,18 @@
+public class Dowhile {
+
+    public static void main(String[] args) {
+        int num=10;
+        do{
+            System.out.println(10);
+            num--;
+
+        }while(num>10);
+        int num1=10;
+        while(num1>10)
+        {
+            System.out.println(num);
+            num--;
+        }
+    }
+    
+}
