2cc9c0e826a060140b39275a6c1ea262afbe7ec6
Thanuja <156277448+ThanujaPinninti@users.noreply.github.com>
2024-07-22 17:50:36 +0530

    Example for Method Overriding

---
+++ Methodoverriding.java
@@ -0,0 +1,28 @@
+Class Shape{
+    void area(){
+        System.out.println("Area of Shape:");
+    }
+}
+Class Rectangle extends Shape{ 
+    int length,breath;
+    public Rectangle(String name, int length,int breath){
+        super(name);
+        this.length= length;
+        this.breath=breath;
+    }
+    void area()
+    {
+        int area=length*breath;
+        System.out.println("Area of Rectangle:"+area);
+    }
+}
+
+
+
+public class Methodoverriding {
+    public static void main(String[] args) {
+        Rectangle rectangle
+    }
+    
+    
+}
